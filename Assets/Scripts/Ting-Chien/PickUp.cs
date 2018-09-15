e4092fa355fa432327b41ddfee4a0c8ae5dc62af
Ting-Chien Chang <tingchie@usc.edu>
2018-09-15 16:17:19 -0700

    Add personal script folder, prefabs folder, pick-up base class script

---
+++ Assets/Scripts/Ting-Chien/PickUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public Vector3 rotate = new Vector3(0, 60, 0);
+    public float floatingSpeed = 2.0f;
+    public float floatingHeight = 0.3f;
+
+    private float yOriginPos;
+    private float count;
+
+	// Use this for initialization
+	void Start () {
+        yOriginPos = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotate * Time.deltaTime);
+
+        Vector3 movement = transform.position;
+        movement.y = evaluatefloatingY();
+        transform.position = movement;
+	}
+
+    float evaluatefloatingY()
+    {
+        count = count + floatingSpeed * Time.deltaTime;
+        if (count > 2 * Mathf.PI) count %= Mathf.PI;
+
+        float offset = floatingHeight * Mathf.Sin(count);
+
+        return yOriginPos + offset;
+    }
+}
