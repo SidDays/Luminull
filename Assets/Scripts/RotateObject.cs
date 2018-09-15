7e57dff36d3c78f7cd3b1aa88d8c27c98fc606f5
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-09-14 19:26:27 -0700

    Mirrors rotate left or right on key press ⌨

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    private static readonly float SPEED = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * SPEED);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * SPEED);
+        }
+    }
+}
