31756462868a2b9ff07db82bfbf78d8c75b10034
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-09-15 13:00:38 -0700

    Moving player script

---
+++ Assets/Scripts/PlayerMoveDefault.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveDefault : MonoBehaviour {
+	private static readonly float PlayerSpeed = 1.125f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.forward * Time.deltaTime * PlayerSpeed;
+
+		// for debugging purposes, reset the position of the cube if the spacebar is pressed
+		if (Input.GetKey(KeyCode.Space)) {
+			transform.position = new Vector3(0, 0.125f, -3.261f);
+		}
+	}
+}
