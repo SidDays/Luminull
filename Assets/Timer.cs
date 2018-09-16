7c76642a680606c50bcfe1e89d51be4a04c08b54
shrutika-ssk <shrutikk@usc.edu>
2018-09-15 18:14:25 -0700

    Initial work on Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime;
+    private bool finnished = false;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (finnished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+
+        if(minutes=="2"){
+            Finnish();
+        }
+
+	}
+
+    public void Finnish() {
+        finnished = true;
+        timerText.color = Color.yellow;
+    }
+}
