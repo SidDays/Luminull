a7589bb08a4ed90c5804dbdb1a582c05ae5e5251
Alexander Longazo <alongazo@usc.edu>
2018-09-18 15:00:35 -0700

    Fixes reflection of player; Adds lasers to detect where player is traveling

---
+++ Assets/Scripts/PlayerCameraChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraChanger : MonoBehaviour {
+    public GameObject MainCamera;
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("CameraChanger"))
+        {
+            
+           // MainCamera.transform.RotateTowards(other.transform.forward);
+        }
+    }
+}
--- Assets/Scripts/PlayerControllerTemp.cs
+++ Assets/Scripts/PlayerControllerTemp.cs
@@ -6,18 +6,35 @@ public class PlayerControllerTemp : MonoBehaviour {
 
     public bool Colliding = false;
     Rigidbody PlayerRB;
+    public GameObject LaserObject;
     public float speed = 15.0f;
-
+    public static float LaserNormalSpeed = 0.5f;
+    public float LaserSpawnSpeed = LaserNormalSpeed;
     void Start()
     {
         PlayerRB = GetComponent<Rigidbody>();
-        
-    }
+        /*GameObject NewLaser = Instantiate(LaserObject);
+        //NewLaser.transform.parent = transform;
+        NewLaser.transform.position = transform.position;
+        NewLaser.transform.forward = transform.forward;
+        Physics.IgnoreCollision(NewLaser.GetComponent<Collider>(), GetComponent<Collider>());
+        */
+     }
 
-    /*void Update()
+    void Update()
     {
+        LaserSpawnSpeed -= Time.deltaTime;
+        if(LaserSpawnSpeed<0.0f)
+        {
+            GameObject NewLaser = Instantiate(LaserObject);
+            //NewLaser.transform.parent = transform;
+            NewLaser.transform.position = transform.position;
+            NewLaser.transform.forward = transform.forward;
+            Physics.IgnoreCollision(NewLaser.GetComponent<Collider>(), GetComponent<Collider>());
+            LaserSpawnSpeed = LaserNormalSpeed;
+        }
         //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = speed * Time.deltaTime;
+        /*var z = speed * Time.deltaTime;
         if (!Colliding)
         {
 
@@ -35,12 +52,31 @@ public class PlayerControllerTemp : MonoBehaviour {
             {
                 transform.Rotate(0, -90, 0);
             }
-        }
-    }*/
+        }*/
+    }
 
     private void FixedUpdate()
     {
         PlayerRB.velocity = transform.forward * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin Pick Up"))
+        {
+            CoinPickUp script = other.GetComponent<CoinPickUp>();
+            script.OnPickUp();
+        }
+
+        if (other.gameObject.CompareTag("Mirror"))
+        {
+            if (!other.gameObject.GetComponent<ReflectObject>().MirrorHit)
+            {
+                Vector3 ReflectionAngle = Vector3.Reflect(transform.forward, other.transform.forward);
+                transform.forward = ReflectionAngle;
+                other.gameObject.GetComponent<ReflectObject>().MirrorHit = true;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
