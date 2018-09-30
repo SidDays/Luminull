da844618fa9ac8376e5efed6d023bb022094dce5
shrutika-ssk <shrutikk@usc.edu>
2018-09-29 20:15:07 -0700

    respawn on hitting wall

---
+++ Assets/Scripts/WallObstacle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallObstacle : MonoBehaviour
+{
+
+    public Transform RedirectPointTransform;
+
+    private Transform PlayerTransform;
+    //private GameStateController GameStateController;
+
+
+    void Start()
+    {
+        PlayerTransform = GameObject.Find("Player").transform;
+    
+        //GameObject game = GameObject.Find("Game State Controller");
+        //if (game == null)
+        //    Debug.LogError("PlayerControllerTemp: Can not find Game State Controller.");
+        //GameStateController = game.GetComponent<GameStateController>();
+    }
+    public void WarpPlayerToRedirectPoint1()
+    {
+        Debug.Log("Inside WarpPlayer method");
+        //if (deathCount > 0)
+        //{
+            //deathCount--;
+            PlayerTransform.position = RedirectPointTransform.position;
+
+            //RedirectDirection object must be first child!
+            Vector3 RedirectDirection = RedirectPointTransform.GetChild(0).forward;
+
+            PlayerTransform.forward = RedirectDirection;
+        //}
+        //else
+        //{
+        //    GameStateController.OnPlayerLose();
+        //}
+
+
+    }
+}
+
+
+
+
+
+
