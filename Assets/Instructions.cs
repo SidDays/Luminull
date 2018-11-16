64e9e9013b47ec330d88f6352eb5ccea939bee11
shrutika-ssk <shrutikk@usc.edu>
2018-11-16 00:47:38 -0800

    Tutorial level functionality added

---
+++ Assets/Instructions.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Instructions : MonoBehaviour {
+
+    public Text instructText;
+    public static int count;
+    public Button OK_button;
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("In start");
+        //OK_button = GetComponent<Button>();
+        OK_button.gameObject.SetActive(false);
+
+        //instructText = GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider checkpoint)
+    {
+        if (checkpoint.gameObject.CompareTag("Player"))
+        {
+
+            Debug.Log("Inside if");   
+            Time.timeScale = 0;
+
+            Debug.Log("After Pause");
+
+
+            if (count == 0)
+            {
+                OK_button.gameObject.SetActive(true);
+                instructText.text = "Button Instructions";
+                count++;
+            }
+            else if (count == 1)
+            {
+                OK_button.gameObject.SetActive(true);
+                instructText.text = "";
+                instructText.text = "Click the button to the left";
+                count++;
+            }
+            else if (count == 2)
+            {
+                OK_button.gameObject.SetActive(true);
+                instructText.text = "";
+                instructText.text = "Click the button to the right";
+                count++;
+            }
+            else if (count == 3)
+            {
+                OK_button.gameObject.SetActive(true);
+                instructText.text = "";
+                instructText.text = "Click the button below";
+                count++;
+            }
+
+        }
+
+
+    }
+
+        
+    public void resumeGame(){
+
+        instructText.text = "";
+
+        Time.timeScale = 1;
+
+        OK_button.gameObject.SetActive(false);
+
+      
+    }
+
+}
