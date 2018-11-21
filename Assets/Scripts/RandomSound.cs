d50c35e9cd171d7c0aed66d61f189f72692143d3
Alexander Longazo <alongazo@usc.edu>
2018-11-21 12:39:50 -0800

    Adds new art and skyboxes

---
+++ Assets/Scripts/RandomSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSound : MonoBehaviour {
+
+    public List<AudioClip> RandomSoundEffects;
+    public AudioSource SoundEffectSource;
+    public bool OnLoop = true;
+
+    private bool SoundEffectPlaying;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(OnLoop)
+        {
+            int RandInt = Random.Range(0, RandomSoundEffects.Count);
+
+            if(RandomSoundEffects.Count > 1 && !SoundEffectSource.isPlaying)
+            {
+                AudioClip NextClip = RandomSoundEffects[RandInt];
+                SoundEffectSource.clip = NextClip;
+                SoundEffectSource.Play();
+            }
+        }
+	}
+}
